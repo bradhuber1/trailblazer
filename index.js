7f6b8865067b3da59e9398f859d76d249e41f0d0
Bradley Huber <bradhuber1@yahoo.com>
2021-05-05 23:24:20 -0600

    Updated navbar, added java conditionals page

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ express()
   .get('/', (req, res) => res.render('pages/home'))
   .get('/fractal', (req, res) => res.render('pages/fractal'))
   .get('/loops', (req, res) => res.render('pages/loops'))
+  .get('/conditionals', (req, res) => res.render('pages/conditionals'))
   .get('/contact', (req, res) => res.render('pages/contact'))
   .get('/about', (req, res) => res.render('pages/about'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
